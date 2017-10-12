5c2417fdf7bed205b55d846303b62a9627c7320e
GG <gbgdev@gmail.com>
2017-10-12 16:01:19 +0200

    Creating VideoList component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyCwN8N2JvYBGbOw-AUPGA7U6GBCTS_rcC4';
 
