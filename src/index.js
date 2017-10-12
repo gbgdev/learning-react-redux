8216f680c91d1b2ab61efcaa445824401cc09fa8
GG <gbgdev@gmail.com>
2017-10-12 13:23:04 +0200

    Creating SearchBar component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCwN8N2JvYBGbOw-AUPGA7U6GBCTS_rcC4';
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
