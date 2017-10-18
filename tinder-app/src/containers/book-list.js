ecc5e11354933d11a92372a9061c69de2e11df92
GG <gbgdev@gmail.com>
2017-10-18 11:58:03 +0200

    Making book-list container work

--- tinder-app/src/containers/book-list.js
+++ tinder-app/src/containers/book-list.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
-  renderlist() {
+class BookList extends Component {
+  renderList() {
     return this.props.books.map((book) => {
-      <li key={book.title} className="list-group-item">{book.title}</li>
+      return (
+        <li key={book.title} className="list-group-item">{book.title}</li>
+      );
     });
   }
+
   render() {
     return (
       <ul className="list-group col-sm-4">
@@ -14,3 +18,11 @@ export default class BookList extends Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
